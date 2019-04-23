04426c39d69b47d1b612ceddf46828556ce694d5
Frederic Köberl <frederic.koeberl@gmail.com>
2019-04-23 18:32:02 +0200

    Add FRDMNSK specific styles.

--- docs/js/index.js
+++ docs/js/index.js
@@ -5,7 +5,11 @@ function start(event) {
     var audioEl = cardEl.getElementsByTagName('audio')[0];
     var videoEl = cardEl.getElementsByTagName('video')[0];
 
-    if (audioEl.readyState === 4) {
+    if (!audioEl.paused) {
+        audioEl.currentTime = 0;
+        audioEl.pause();
+        cardEl.classList.remove('card--show-giphy');
+    } else if (audioEl.readyState === 4) {
         play();
     } else {
         cardEl.classList.add('card--loading');
@@ -15,6 +19,7 @@ function start(event) {
     }
 
     function play() {
+        stopOthers();
         audioEl.addEventListener('ended', clipEndedCallback);
         audioEl.play();
         videoEl.play();
@@ -28,5 +33,16 @@ function start(event) {
 
     function clipEndedCallback() {
         cardEl.classList.remove('card--show-giphy');
+    }
+
+    function stopOthers() {
+        var cards = document.querySelectorAll('.card');
+        cards.forEach(function (cardEl) {
+            var audioEl = cardEl.querySelector('audio');
+            if (!audioEl || audioEl.paused) return;
+            audioEl.currentTime = 0;
+            audioEl.pause();
+            cardEl.classList.remove('card--show-giphy');
+        });
     }
 }
